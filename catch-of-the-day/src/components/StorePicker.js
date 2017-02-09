e0016c36645bc71e69319249223bf7e824f95662
Sam Goldsmith <samggold@gmail.com>
2017-02-09 09:47:34 -0700

    add method to route to storeid

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,17 @@
 import React from 'react';
 //import { render } from 'react-dom';
 import { getFunName } from '../helpers';
-//will not import entire react-dom library
 
 class StorePicker extends React.Component {
+  goToStore(event) {
+    event.preventDefault();
+    //grab text from input field
+    //route from / to /store/:storeid
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
         <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store</button>
